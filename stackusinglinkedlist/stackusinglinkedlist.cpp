8435a99c87ef49be4fd52ac96b9ee588088d6846
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-11 09:45:33 +0700

    void pop

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Node class representing a single node in the linked list
-
 class Node {
 
 public:
@@ -14,27 +12,32 @@ public:
 	}
 };
 
-// Stack class
 
-class Stack {
-
+class stack {
 private:
-
-	Node* top; // Pointer to the top node of the stack
+	Node* top;
 
 public:
-	Stack() {
-		top = NULL; // Initialize the stack with a null top pointer
-	}
+	stack() {
+		top = NULL;
 
-	// Push operation: Insert an element onto the top of the stack
+	}
 	int push(int value) {
-		Node* newNode = new Node(); // 1. Allocate memory for the new node
-		newNode->data = value; // 2. assign value
-		newNode->next = top; // 3. Set the next pointer of the new node to the
-		top = newNode; //4 Update the top pointer to the new node
-		cout << "Push value: " << value << endl;
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "push value: " << value << endl;
 		return value;
 	}
 
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
 
+	}
+
+	

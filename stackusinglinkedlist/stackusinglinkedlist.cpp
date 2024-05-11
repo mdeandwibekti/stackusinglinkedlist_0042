fedc58d87727db8f1f6bf17dea177d80c2190228
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-11 09:46:56 +0700

    void peek else

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -43,6 +43,16 @@ public:
 		if (top == NULL) {
 			cout << "List is empty." << endl;
 		}
+		else {
+			Node* current = top;
+			while (current != NULL) {
+				cout << current->data << " " << endl;
+				current = current->next;
+			}
+			cout << endl;
+		}
+	}
+	
 
 		
 

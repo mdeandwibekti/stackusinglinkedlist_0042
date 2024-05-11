291aa5d2e90835af039e87a60e0538b5db1c5232
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-11 09:48:35 +0700

    int main stack

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -57,7 +57,22 @@ public:
 	}
 
 };
+int main() {
+	stack stack;
 
+	int choice = 0;
+	int value;
+
+	while (choice != 5) {
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Peek\n";
+		cout << "4. Exit\n";
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+
+		
 	
 
 		

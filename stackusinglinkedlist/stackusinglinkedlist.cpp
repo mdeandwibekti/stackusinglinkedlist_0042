d684ee23027aec8432726356fdb14472f8d6c5cf
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-11 09:29:34 +0700

    class stack

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // Node class representing a single node in the linked list
 
 class Node {
+
 public:
 	int data;
 	Node* next;
@@ -12,5 +13,18 @@ public:
 		next = NULL;
 	}
 };
+
+// Stack class
+
+class Stack {
+
+private:
+
+	Node* top; // Pointer to the top node of the stack
+
+public:
+	Stack() {
+		top = NULL; // Initialize the stack with a null top pointer
+	}
 
 
